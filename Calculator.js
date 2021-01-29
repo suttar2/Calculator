6bef0580452d882b7908ce59bbc0a001b1e3aaea
Nathan <suttar@gmail.com>
2021-01-28 21:34:14 -0500

    Cleaned up functions. Running into problem with extra value appearing in memory array

--- Calculator.js
+++ Calculator.js
@@ -25,21 +25,12 @@ function clearMemory(){ memory = [] };
 //pushes the information on the display into the memory
 function commitLast(){ memory.push(displayVal.textContent) };
 
-//checks the type of the last item in the array. 
-function checkLastPress() {console.log(memory[-1].typeof)};
-
-//Thought I should make a function to check for integers - realized it was better to make one that checks the type
-//function checkInt(){typeOf( memory[-1] == int );
-
-//Thought I should make a function that checks if something in the memory is a function - same as above
-//function checkFunc(){ }
-
 //calculate function - takes an array with at least 3 items. and runs the operate function on it using the operator in array position 1 
 function calculate(arr){
     let result = operate(arr[1], parseInt(arr[0]), parseInt(arr[2]))
-    memory.splice( 0, 3 , parseInt(result) );
-
+    memory.splice( 0, 3 , result );
 };
+
     //maybe add the below later?
     //loop over the memory array using operate
     //operate using memory[0] and memory[2] with operator at memory[1]
@@ -70,47 +61,40 @@ let pressButton = ButtonPanel.addEventListener('click', function(btn) {
     //use this as model for all other operations once working
     if (buttonPressed == "+") {
         commitLast();
-        memory.push(add);
         clearScreen();
+        memory.push(add);
     };
 
     //same as above
     if (buttonPressed == "X"){
         commitLast();
-        memory.push(multiply);
         clearScreen();
+        memory.push(multiply);
     };
 
     //if equal is pressed, commit the displayVal to memory, Clear the screen, and then calculate the memory. 
     if(buttonPressed == "="){
-        console.log(memory);
 
-//        if (checkLastPress !== null){commitLast()};
-//        if (memory[1].typeOf == {commitLast()})
         commitLast();
-
         clearScreen();
-        
-        console.log(memory[1].typeOf)
+        console.table(memory);
 
         calculate(memory);
-        
-        console.log(memory);
-        
+       
         displayVal.textContent = memory[0];
-//        if memory.length console.log(typeof(memory[0]))
+
+        console.log(memory)
+
     };
 
 });
 
 
-//console.log(operate(add, 34, 2)); // calling the function directly seems to actually add
 
 
 
 
-
-
+    // old stuff
     // if (buttonPressed == "+"){
     //     numMemory.push(displayVal.textContent);
     //     clearScreen();
