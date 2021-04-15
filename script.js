0712833a3f8314f0de95377b5e325f30ccc79bd9
jcrops <suttar@gmail.com>
2021-04-15 10:51:41 -0400

    Updated auto calculation when operation keys are pressed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //listen on the button panel and listen on the display
-const ButtonPanel = document.getElementById('ButtonPanel');
+const buttonPanel = document.getElementById('ButtonPanel');
 const displayVal = document.getElementById("NumDisplay");
 
 //functions that add, subtract, multiply divide, and operate
@@ -33,9 +33,14 @@ function doOperator(func){
 //calculate function - takes an array with at least 3 items. and runs the operate function on it using the operator in array position 1 
 function calculate(arr){
     if (arr.length >= 3) {
-        let result = operate(arr[1], parseInt(arr[0]), parseInt(arr[2]))
+        let result = operate(arr[1], parseFloat(arr[0]), parseFloat(arr[2]))
+        
         arr.splice( 0, 3 , result );
-        if (arr.length > 2) {calculate(arr)};
+        
+        if (arr.length > 2) {
+            calculate(arr)
+        };
+        displayVal.textContent = memory[0]
     };
 };
 
@@ -46,49 +51,47 @@ function calculate(arr){
 
 
 //make a new variable called pressButton, it get's defined when a button is pressed. It listens for a click on a button
-let pressButton = ButtonPanel.addEventListener('click', function(btn) {
+let pressButton = buttonPanel.addEventListener('click', function(btn) {
     //button pressed get's defined as the ID of the button being pressed. 
+    
     let buttonPressed = btn.target.id;
 
     if (memory.length >= 3){calculate(memory)};
 
-    // if it's a number value our display will show it and if isn't empty, concat it
     if (buttonPressed >= 0 && buttonPressed <= 9) {
-    displayVal.textContent = displayVal.textContent + buttonPressed
+        displayVal.textContent = displayVal.textContent + buttonPressed
+    };// if it's a number value our display will show it and if isn't empty, concat it
+
+    if (buttonPressed === ".") {
+        displayVal.textContent = displayVal.textContent + "."
+    };
+
+    if (buttonPressed === "C") {
+        clearScreen(); //if it's clear, clear the screen
     };
     
-    //if it's clear, clear the screen
-    if (buttonPressed == "C") {
-        clearScreen();
-    };
-    //if it's clear E clear screen and memory. Is that what that button does on regular calculators?
-    if (buttonPressed == "CE") {
+    if (buttonPressed === "CE") {
         clearScreen();
         clearMemory();
-    };
+    };//if it's clear E clear screen and memory. Is that what that button does on regular calculators?
 
     //if it's adding, commit the value in displayval to memory commit the add function to memory, and then clear the screen
     //use this as model for all other operations once working - did that. it works
-    if (buttonPressed == "+") {doOperator(add)};
-    if (buttonPressed == "-") {doOperator(subtract)};
-    if (buttonPressed == "X") {doOperator(multiply)};
-    if (buttonPressed == "/") {doOperator(divide)};
-    if (buttonPressed == "%") {doOperator(modulus)};
+    if (buttonPressed === "+") {doOperator(add)};
+    if (buttonPressed === "-") {doOperator(subtract)};
+    if (buttonPressed === "X") {doOperator(multiply)};
+    if (buttonPressed === "/") {doOperator(divide)};
+    if (buttonPressed === "%") {doOperator(modulus)};
 
     //if equal is pressed, commit the displayVal to memory, Clear the screen, and then calculate the memory. 
-    if(buttonPressed == "="){
+    if(buttonPressed === "="){
         if (memory.length > 1){
 
         commitLast();
-
         clearScreen();
-    
         calculate(memory);
-       
         displayVal.textContent = memory[0];
-
         clearMemory();
-
         };
         
        
@@ -96,29 +99,3 @@ let pressButton = ButtonPanel.addEventListener('click', function(btn) {
     };
 
 });
-
-
-
-
-
-
-    // old stuff
-    // if (buttonPressed == "+"){
-    //     numMemory.push(displayVal.textContent);
-    //     clearScreen();
-    //     }
-
-    //     if (numMemory.length > opMemory.length){
-    //         opMemory.push(add);            
-    //     }
-    //     console.log(numMemory);
-    //     console.log(opMemory);
-
-        // if (buttonPressed == "+") {
-    //         memory.push(
-    //             displayVal.textContent,
-    //             add());
-    //     clearScreen();
-
-    //     console.log(memory);
-    // };
